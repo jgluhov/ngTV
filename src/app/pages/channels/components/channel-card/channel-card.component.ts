7a435aa7e86c25862dd2cb420ff4dfd850181b46
Evgeny Glukhov <jgluhov@gmail.com>
2019-04-30 11:42:49 +0300

    add hover effect

--- src/app/pages/channels/components/channel-card/channel-card.component.ts
+++ src/app/pages/channels/components/channel-card/channel-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 import { IChannel } from '@interfaces/channel.interface';
 
 @Component({
